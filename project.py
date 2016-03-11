7036ccb6fdb8d623bfaa8e157ccde203d93d3f30
saurav kumar <saurav24007@gmail.com>
2016-03-12 00:36:16 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,12 +2,13 @@ import re
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import sentiwordnet as swn
+from nltk.corpus import wordnet_ic
+
 
 def tagger(sentence):
     text = sentence.split()
     taggedText = nltk.pos_tag(text)
-
-    print(taggedText)
+    #print(taggedText)
     return taggedText
 
 
@@ -18,10 +19,17 @@ def sentiScores(word):      #takes input as string, return bith +&- values
     values.append(word.neg_score())     #negative value
     return values
 
-def similarity(category):
-	
 
-	pass
+def similarity(word1, word2):
+	obj1 = wn.synset(word1 + ".n.01")
+	obj2 = wn.synset(word2 + ".n.01")
+	#print(obj1)
+	brown_ic = wordnet_ic.ic('ic-brown.dat') 	# Information content
+	semcor_ic = wordnet_ic.ic('ic-brown.dat')
+	
+	value = obj1.res_similarity(obj2, brown_ic)
+	
+	return value
 
 
 text = input()
@@ -39,26 +47,44 @@ for i in range(len(Taggedtext)):
     match = re.match(r'VB*', old)
     if match:
         Taggedtext[i] = (Taggedtext[i][0],'VB')
-        listV.append(TaggedText[i][0])
+
+        listV.append(Taggedtext[i][0])
+
         countV += 1
+        
 #for noun
     match = re.match(r'NN*', old)
     if match:
         Taggedtext[i] = (Taggedtext[i][0],'NN')
         countN += 1
-        listN.append(TaggedText[i][0])
+
+        listN.append(Taggedtext[i][0])
+
 #for adjectives
     match = re.match(r'JJ*', old)
     if match:
         Taggedtext[i] = (Taggedtext[i][0],'JJ')
         countA += 1
-        listA.append(TaggedText[i][0])
+
+        listA.append(Taggedtext[i][0])
+
+
 #for adverb
     match = re.match(r'RB*', old)
     if match:
         Taggedtext[i] = (Taggedtext[i][0],'RB')
         countR += 1
-        listR.append(TaggedText[i][0])
+
+        listR.append(Taggedtext[i][0])
 
 
-print(Taggedtext)
+print(After applying the pos tags,Taggedtext)
+
+for i in Taggedtext:
+    val = similarity('good','a')
+    print(val)
+
+    listR.append(Taggedtext[i][0])
+
+
+#print(Taggedtext)
